b5916392bf516551fcd682b96157c591dd65cb86
Kfir <kfirprods@gmail.com>
2020-02-08 17:31:30 +0200

    Renamed remainders of spain-blog from the old cloned project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('spain-blog app is running!');
+    expect(page.getTitleText()).toEqual('argentina-blog app is running!');
   });
 
   afterEach(async () => {
