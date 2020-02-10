0a754cc5bbff98441f49df0a9aae9a296b987ade
Kfir <kfirprods@gmail.com>
2020-02-10 22:46:22 +0200

    Removed more unused code; Bigger text in destination-posts-view;

--- src/app/models/gallery-image.type.ts
+++
@@ -1,4 +0,0 @@
-export interface GalleryImage {
-  source: string;
-  description: string;
-}
--- src/app/models/trip-duration.ts
+++
@@ -1,6 +0,0 @@
-import { firestore } from 'firebase';
-
-export interface TripDuration {
-  tripStart: firestore.Timestamp;
-  tripEnd: firestore.Timestamp;
-}
