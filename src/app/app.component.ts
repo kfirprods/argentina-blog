bee6876e84456cf7e998d1848f4290d982b443a0
Kfir <kfirprods@gmail.com>
2020-02-21 02:34:25 +0200

    implemented cool main loading animation, need to apply to child components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, Injectable, ViewEncapsulation } from '@angular/core';
 
 @Component({
@@ -8,4 +9,10 @@ import { Component, Injectable, ViewEncapsulation } from '@angular/core';
 })
 @Injectable()
 export class AppComponent {
+  isLoading: boolean;
+
+  constructor() {
+    this.isLoading = false;
+    // TODO: set loading to true based on event raised by child components
+  }
 }
